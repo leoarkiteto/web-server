007e3c0c99bd1e9d2474d222a4532703ae3cb039
Leo Franca <leoarkiteto@gmail.com>
2021-11-18 17:04:47 +0000

    🚧 (Section 7): Serving up HTML and JSON

--- src/app.js
+++ src/app.js
@@ -2,13 +2,37 @@ const express = require('express');
 
 const app = express();
 
+// app.com
 app.get('', (req, res) => {
-  res.send('Hello express!');
+  res.send('<h1>Weather</h1>');
 });
 
-// app.com
 // app.com/help
+app.get('/help', (req, res) => {
+  res.send([
+    {
+      name: 'Andrew',
+      age: 27,
+    },
+    {
+      name: 'Leo',
+      age: 37,
+    },
+  ]);
+});
+
 // app.com/about
+app.get('/about', (req, res) => {
+  res.send('<h1>About Page</h1><p>This section is under construction...</p>');
+});
+
+// app.com/weather
+app.get('/weather', (req, res) => {
+  res.send({
+    forecast: 'It is snowing',
+    location: 'Philadelphia',
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server is up on port: 3000.');
