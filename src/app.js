ee27b2bd82db8d589de7a7fab46ef9ea0f90b151
Leo Franca <leoarkiteto@gmail.com>
2021-11-18 17:27:33 +0000

    🚧 (Section 7): Serving up Static Assets

--- src/app.js
+++ src/app.js
@@ -1,30 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-// app.com
-app.get('', (req, res) => {
-  res.send('<h1>Weather</h1>');
-});
-
-// app.com/help
-app.get('/help', (req, res) => {
-  res.send([
-    {
-      name: 'Andrew',
-      age: 27,
-    },
-    {
-      name: 'Leo',
-      age: 37,
-    },
-  ]);
-});
-
-// app.com/about
-app.get('/about', (req, res) => {
-  res.send('<h1>About Page</h1><p>This section is under construction...</p>');
-});
+app.use(express.static(publicDirectoryPath));
 
 // app.com/weather
 app.get('/weather', (req, res) => {
